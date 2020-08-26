64fcbe5d16312added473920340d322d93720950
Bogdan Beley <bogdan.beley@outlook.com>
2020-08-26 21:30:18 +0300

    Add new actions to CategoriesController and ProductsController

--- IShop/App_Start/WebApiConfig.cs
+++ IShop/App_Start/WebApiConfig.cs
@@ -12,6 +12,11 @@ namespace IShop
     {
         public static void Register(HttpConfiguration config)
         {
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(
+                new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(
+                new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream"));
+
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
--- IShop/Controllers/ProductsController.cs
+++ IShop/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using IShop.BusinessLogic.Services;
+using IShop.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,14 +14,40 @@ namespace IShop.Controllers
     {
         private IProductService _productService = new ProductService();
 
+        [HttpGet]
         public IHttpActionResult GetAll()
         {
             return Ok(_productService.GetAll());
         }
 
+        [HttpGet]
         public IHttpActionResult Get(int id)
         {
             return Ok(_productService.Get(id));
+        }
+
+        [HttpPost]
+        public IHttpActionResult Add([FromBody] Product product)
+        {
+            _productService.Add(product);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public IHttpActionResult Update([FromBody] Product product)
+        {
+            _productService.Update(product);
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            _productService.Delete(id);
+
+            return Ok();
         }
     }
 }
