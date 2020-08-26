eaf3a98e66256f548a88b9d84c39d1c66b3be2fc
Bogdan Beley <bogdan.beley@outlook.com>
2020-08-26 12:28:37 +0300

    Add projects IShop.Domain and IShop.BusinessLogic

--- IShop.Domain/Models/Category.cs
+++ IShop.Domain/Models/Category.cs
@@ -4,7 +4,6 @@ namespace IShop.Domain.Models
     public class Category : IIdentifiable
     {
         public int Id { get; set; }
-
         public string Name { get; set; }
     }
 }
--- IShop.Domain/Models/Product.cs
+++ IShop.Domain/Models/Product.cs
@@ -4,11 +4,8 @@ namespace IShop.Domain.Models
     public class Product : IIdentifiable
     {
         public int Id { get; set; }
-
         public int CategoryId { get; set; }
-
         public string Name { get; set; }
-
         public double Price { get; set; }
     }
 }
