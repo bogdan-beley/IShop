9ef200e8361efb2179d4dfc772f31862cf9ee85d
Bogdan Beley <bogdan.beley@outlook.com>
2020-08-26 12:41:19 +0300

    Add CategoriesController and ProductsController

---
+++ IShop/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using IShop.BusinessLogic.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Filters;
+
+namespace IShop.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private IProductService _productService = new ProductService();
+
+        public IHttpActionResult GetAll()
+        {
+            return Ok(_productService.GetAll());
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            return Ok(_productService.Get(id));
+        }
+    }
+}
