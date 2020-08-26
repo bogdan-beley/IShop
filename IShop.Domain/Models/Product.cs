e3e6821558aed71c0f3af4dcc5fe5bb687f1218c
Bogdan Beley <bogdan.beley@outlook.com>
2020-08-26 12:19:48 +0300

    Update packages

---
+++ IShop.Domain/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿
+namespace IShop.Domain.Models
+{
+    public class Product : IIdentifiable
+    {
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+    }
+}
